efed3071941f5a182c44d81a02d696508b2e8f17
Darley Rodrigues <darleyramos96@gmail.com>
2025-05-15 17:13:27 -0300

    Commit inicial do projeto arquiteturaspring

---
+++ src/main/java/com/darleyrodrigues/arquiteturaspring/ArquiteturaspringApplication.java
@@ -0,0 +1,13 @@
+package com.darleyrodrigues.arquiteturaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArquiteturaspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArquiteturaspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/darleyrodrigues/arquiteturaspring/ArquiteturaspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darleyrodrigues.arquiteturaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArquiteturaspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
