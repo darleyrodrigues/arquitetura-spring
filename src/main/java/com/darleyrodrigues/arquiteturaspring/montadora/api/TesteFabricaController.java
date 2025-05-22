b85215a4931dc7657686667270b6b47dfb4257d9
Darley Rodrigues <darleyramos96@gmail.com>
2025-05-22 10:25:19 -0300

    Adição de annotation

--- src/main/java/com/darleyrodrigues/arquiteturaspring/montadora/api/TesteFabricaController.java
+++ src/main/java/com/darleyrodrigues/arquiteturaspring/montadora/api/TesteFabricaController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TesteFabricaController {
 
     @Autowired
-    @Qualifier("motorEletrico")
+    @Eletrico
     private Motor motor;
 
     @PostMapping
